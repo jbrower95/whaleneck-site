c8a7ecca3e64e8c35fd52c911170eb08a6610079
Justin Brower <justin_brower@brown.edu>
2016-01-06 14:08:15 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -3,4 +3,27 @@ $(document).ready(function() {
 
 	new WOW().init();
 
+
+	function moveDiv() {
+    var $span = $("#whale_image");
+
+    	var ul_height = $("#nav_bar").height();
+
+		var maxLeft = $(window).width() - $span.width();
+        var maxTop = ($(window).height() / 2) - $span.height() - ul_height;
+        var leftPos = Math.floor(Math.random() * (maxLeft + 1))
+        var topPos = Math.floor(Math.random() * (maxTop + 1)) + ul_height
+
+		$span.animate({left: leftPos, top: topPos});
+	};
+
+
+	$("#whale_image").click(function() {
+		
+	});
+
+
+	moveDiv();
+	setInterval(moveDiv, 500);
+
 });	
